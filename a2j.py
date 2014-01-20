5640e30584e13c66ecabc5f934104dd76378f85c
Moishe Lettvin <mlettvin@etsy.com>
2014-01-20 11:00:00 -0700

    Add command line args; don't use custom html2text

--- a2j.py
+++ a2j.py
@@ -1,7 +1,10 @@
+import argparse
 import codecs
 import feedparser
 import html2text
+import os
 import re
+import sys
 import unicodedata
 
 # Stolen from django
@@ -16,44 +19,70 @@ def slugify(value):
     value = re.sub(' ', '_', value)
     return value
 
+def getmarkdown(s):
+    # disable line-wrapping on html2text output
+    h = html2text.HTML2Text()
+    h.body_width = 0
+    h.escape_snob = 1
+    return h.handle(s)
 
-d = feedparser.parse('input.xml')
+parser = argparse.ArgumentParser(description="Convert feed file (from eg. Blogger Takeout) to Jekyll _posts directory")
+parser.add_argument('--source', '-s', type=argparse.FileType('r'), nargs='?', default=sys.stdin,
+                    help='The source atom file.')
+parser.add_argument('--dest', '-d', required=True,
+                    help='The path to the _posts directory for output, eg. ~/src/foo.github.io/_posts')
+parser.add_argument('--create-dir', '-c', action='store_true', default=False,
+                    help='If true, and the output directory doesn\'t exist, create it. Will not delete existing directory.')
+parser.add_argument('--force-overwrite', '-f', action='store_true', default=False,
+                    help='If true, overwrite existing files. If false, skip existing files.')
+
+args = parser.parse_args()
+
+if not os.path.exists(args.dest):
+    if args.create_dir:
+        os.mkdir(args.dest)
+    else:
+        sys.stderr.write("ERROR: Destination path (%s) does not exist. Use --create-dir to force create.\n" % args.dest)
+        sys.exit(1)
+
+d = feedparser.parse(args.source)
 
 posts = []
 
-# disable line-wrapping on html2text output
-html2text.BODY_WIDTH = 0
-html2text.ESCAPE_SNOB = 1
-html2text.IGNORE_IMAGES = 1
-
 for entry in d.entries:
-  try:
-    if (entry.tags[0].term == u'http://schemas.google.com/blogger/2008/kind#post' and
-        not hasattr(entry, 'app_draft') or entry.app_draft != u'yes'):
-      posts.append(entry)
-  except AttributeError:
-    pass
+    try:
+        if (entry.tags[0].term == u'http://schemas.google.com/blogger/2008/kind#post' and
+            not hasattr(entry, 'app_draft') or entry.app_draft != u'yes'):
+            posts.append(entry)
+    except AttributeError:
+        pass
 
 print 'Converting %s posts.' % len(posts)
 
 for post in posts:
-  date = post.published_parsed
-  if post.title:
-    title = slugify(post.title)
-  else:
-    title = 'untitled'
-  filename = '_posts/%s-%s-%s-%s.md' % (date.tm_year, date.tm_mon, date.tm_mday, title)
-  front_matter = '\n'.join((
-    '---',
-    'layout: post',
-    'title: %s' % html2text.html2text(post.title),
-    '---\n'))
-  f = codecs.open(filename, 'w', 'utf-8')
-  f.write(front_matter)
+    date = post.published_parsed
+    if post.title:
+        title = slugify(post.title)
+    else:
+        title = 'untitled'
 
-  content = post.content[0].value
-  if "Helen" in content:
-    print content
-  f.write(html2text.html2text(content))
-  f.close()
+    filename = '%s/%s-%s-%s-%s.md' % (args.dest, date.tm_year, date.tm_mon, date.tm_mday, title)
+    if os.path.exists(filename):
+        if args.force_overwrite:
+            print "overwriting %s" % filename
+        else:
+            print "skipping existing file %s" % filename
+            continue
+    else:
+        print "creating %s" % filename
 
+    front_matter = '\n'.join((
+                '---',
+                'layout: post',
+                'title: %s' % getmarkdown(title),
+                '---\n'))
+    f = codecs.open(filename, 'w', 'utf-8')
+    f.write(front_matter)
+
+    f.write(getmarkdown(post.content[0].value))
+    f.close()
